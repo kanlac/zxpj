a13b9790c5fa78e013143d6df2005595a7335682
serfusE <serfuse@gmail.com>
2018-09-29 16:10:41 +0800

    normal update

--- my.sql
+++ my.sql
@@ -77,7 +77,6 @@ CREATE TABLE Sale (
 );
 
 
-
 INSERT INTO Manager (`username`, `password`, `mobile`, `email`, `status`) VALUE ('Zimmerman', 23444, 18022258888, 'abc@gmail.com', 1);
 
 INSERT INTO `Commodity` (`commodity_id`, `isbn`, `name`, `purchase_price`, `unit`) VALUES ('1', '11', '牛奶', '3', '250毫升');
@@ -95,14 +94,26 @@ INSERT INTO `Commodity` (`commodity_id`, `isbn`, `name`, `purchase_price`, `unit
 INSERT INTO `Commodity` (`commodity_id`, `isbn`, `name`, `purchase_price`, `unit`) VALUES ('13', '19', '早餐面包', '7', '100克');
 INSERT INTO `Commodity` (`commodity_id`, `isbn`, `name`, `purchase_price`, `unit`) VALUES ('14', '20', '达利小面包', '7', '100克');
 
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('1', '番茄酱', '佛山', '311230', '11@qq.com', '', '13011112222');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('2', '黑胡椒酱', '佛山', '322123', '22@qq.com', '', '13022223333');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('3', '挂面', '佛山', '222222', '33@qq.com', '', '13022224444');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('4', '龙须面', '广东', '333333', '44@qq.com', '', '13055554444');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('5', '意大利细面条', '广东', '222222', '12@qq.com', '', '13044444444');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('6', '意大利螺旋面', '广东', '555555', '13@qq.com', '', '13055554444');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('7', '魔芋面', '湖北', '555444', '14@qq.com', '', '13088887777');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('8', '荞麦面', '湖北', '444444', '15@qq.com', '', '13088887777');
-INSERT INTO `Vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('9', '全麦面', '湖北', '666666', '16@qq.com', '', '13055554444');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('1', '小艺', '佛山', '311230', '11@qq.com', '', '13011112222');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('2', '肖尔', '佛山', '322123', '22@qq.com', '', '13022223333');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('3', '小思', '佛山', '222222', '33@qq.com', '', '13022224444');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('4', '小舞', '广东', '333333', '44@qq.com', '', '13055554444');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('5', '小柳', '广东', '222222', '12@qq.com', '', '13044444444');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('6', '小琪', '广东', '555555', '13@qq.com', '', '13055554444');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('7', '小芭', '湖北', '555444', '14@qq.com', '', '13088887777');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('8', '萧九', '湖北', '444444', '15@qq.com', '', '13088887777');
+INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `postal_code`, `email`, `note`, `mobile`) VALUES ('9', '小诗', '湖北', '666666', '16@qq.com', '', '13055554444');
+
+INSERT INTO `client` (`client_id`, `name`, `address`, `postal_code`, `email`, `mobile`) VALUES ('1', '一一', '天津', '311203', '11@qq.com', '13011112222');
+INSERT INTO `client` (`client_id`, `name`, `address`, `postal_code`, `email`, `mobile`) VALUES ('2', '尔尔', '湖北', '311203', '11@qq.com', '13011112222');
+INSERT INTO `client` (`client_id`, `name`, `address`, `postal_code`, `email`, `mobile`) VALUES ('3', '散散', '北京', '311203', '11@qq.com', '13011112222');
+INSERT INTO `client` (`client_id`, `name`, `address`, `postal_code`, `email`, `mobile`) VALUES ('4', '思思', '浙江', '311203', '11@qq.com', '13011112222');
+INSERT INTO `client` (`client_id`, `name`, `address`, `postal_code`, `email`, `mobile`) VALUES ('5', '五五', '福建', '311203', '11@qq.com', '13011112222');
+INSERT INTO `client` (`client_id`, `name`, `address`, `postal_code`, `email`, `mobile`) VALUES ('6', '六六', '天津', '311203', '11@qq.com', '13011112222');
+
+INSERT INTO `purchase` (`purchase_id`, `number`, `date`, `cost`, `quantity`, `status`, `commodity_id`, `vendor_id`, `manager_id`) VALUES ('1', '1', '20180928', '2', '4', '1', '1', '1', '1');
+INSERT INTO `purchase` (`purchase_id`, `number`, `date`, `cost`, `quantity`, `status`, `commodity_id`, `vendor_id`, `manager_id`) VALUES ('2', '2', '20180930', '6', '7', '1', '2', '2', '3');
+INSERT INTO `purchase` (`purchase_id`, `number`, `date`, `cost`, `quantity`, `status`, `commodity_id`, `vendor_id`, `manager_id`) VALUES ('3', '3', '20180927', '10', '9', '1', '3', '3', '2');
+INSERT INTO `purchase` (`purchase_id`, `number`, `date`, `cost`, `quantity`, `status`, `commodity_id`, `vendor_id`, `manager_id`) VALUES ('4', '4', '20180922', '11', '10', '1', '4', '4', '1');
 
 SELECT * FROM Commodity;
